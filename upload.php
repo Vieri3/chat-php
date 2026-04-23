19dbce0b271f972c100ae6fc23350df84f3645f7
Vieri3 <egordohlyj4@gmail.com>
2026-04-23 13:56:46 +0300

    double удаляем файл за ненадобностью

--- upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-// скрипт который загружает в папку /temp на сервере файл отправленный от пользователя
-if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
-    $tempFile = $_FILES['file']['tmp_name'];
-    $targetPath = 'temp/' . $_FILES['file']['name'];
-    move_uploaded_file($tempFile, $targetPath);
-}
